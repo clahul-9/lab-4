632afd1a5499638c298a892f482eaa74283eb7b9
Clara Hultén <clahul-9@student.ltu.se>
2019-11-29 11:18:53 +0100

    4

---
+++ Task-1/Task-1/MotorVehicle.cpp
@@ -0,0 +1,43 @@
+#include "MotorVehicle.h"
+#include "Body.h"
+#include "engine.h"
+#include "owner.h"
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+MotorVehicle::MotorVehicle()
+{
+	engine motor;
+	Body body;
+	owner person;
+	int numbersOfTiers;
+	std::string model;
+	float *tierDiameters = new float[numbersOfTiers];
+	for (int i = 0; i < numbersOfTiers; i++)
+	{
+		tierDiameters[i] = rand() % 100;
+	}
+	
+}
+
+void MotorVehicle::print() {
+	cout << "Motor:";
+	motor.print();
+	cout << "Body:";
+	body.print();
+	cout << "Owner:";
+	person.print();
+	cout << "model:" << model << endl;
+	for (int i = 0; i < numbersOfTiers; i++)
+	{
+		cout << "tier " << i + 1 << ":" << tierDiameters[i];
+	}
+}
+
+
+MotorVehicle::~MotorVehicle()
+{
+}
